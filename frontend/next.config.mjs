a082c75cf6f48fc5bc4ce0afb6622a0803873d70
C N <147255377+Candra0x6@users.noreply.github.com>
2025-03-13 22:58:58 +0700

    Add UI components and update project configuration

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_NFT_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS,
+    NEXT_PUBLIC_MARKET_CONTRACT_ADDRESS:
+      process.env.NEXT_PUBLIC_MARKET_CONTRACT_ADDRESS,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+      {
+        protocol: 'http',
+        hostname: '*',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
