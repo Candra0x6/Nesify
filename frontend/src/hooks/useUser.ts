5214ef125e65a8356fc159056800725d0d902265
C N <147255377+Candra0x6@users.noreply.github.com>
2025-03-13 23:37:56 +0700

    refactor: Replace custom localStorage utility with native localStorage methods in useUser hook

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -1,6 +1,5 @@
 "use client";
 
-import { getLocalStorage } from "@/lib/utils/storage";
 import { useEffect, useState } from "react";
 
 interface User {
@@ -12,8 +11,8 @@ export const useUser = () => {
   const [user, setUser] = useState<User>();
 
   useEffect(() => {
-    const id = getLocalStorage("user_id");
-    const role = getLocalStorage("user_role");
+    const id = localStorage.getItem("user_id");
+    const role = localStorage.getItem("user_role");
     setUser({ id, role });
   }, []);
 
